dfae30b6b1ff468d0e4b83db81ee447995c61c44
daominhkhanh20 <khanhc1k36@gmail.com>
2022-06-08 09:50:01 +0700

    update

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,10 +1,10 @@
-mkdir -p assert/data
+mkdir -p assets/data
 wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=khanhbk20&password=Khanhbk20@123&submit=Login' https://www.cityscapes-dataset.com/login/
-wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1 -P assert/data
-unzip assert/data/gtFine_trainvaltest.zip -d assets/data/
-find assert/data -maxdepth 1 -type f -delete
-wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3 -P assert/data
-unzip assert/data/leftImg8bit_trainvaltest.zip -d assets/data/
-find assert/data -maxdepth 1 -type f -delete
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1 -P assets/data
+unzip assets/data/gtFine_trainvaltest.zip -d assets/data/
+find assets/data -maxdepth 1 -type f -delete
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3 -P assets/data
+unzip assets/data/leftImg8bit_trainvaltest.zip -d assets/data/
+find assets/data -maxdepth 1 -type f -delete
 rm cookies.txt
 rm index.html
