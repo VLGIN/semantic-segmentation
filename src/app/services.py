294c30845b18d6405a5f737fe50c9dd1860da34d
Phạm Văn Nam <57164967+phamnam1805@users.noreply.github.com>
2022-06-19 19:21:46 +0700

    update service

--- src/app/services.py
+++ src/app/services.py
@@ -1,19 +1,18 @@
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request, Response, Header
+from fastapi.staticfiles import StaticFiles
 from dotenv import dotenv_values
 from fastapi.responses import FileResponse
 
 CONFIG = dotenv_values("./.env")
-app = FastAPI()
 
-@app.post("/upload_video/")
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.post("/upload_file/")
 def upload_file(file: UploadFile = File(...)):
     pass
-
-@app.get("/get_file/")
-def get_file():
-    pass
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8001)
