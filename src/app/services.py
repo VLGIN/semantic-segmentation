1ad200162ea555812233e52879f7483790708a1f
Phạm Văn Nam <57164967+phamnam1805@users.noreply.github.com>
2022-06-19 19:29:02 +0700

    add api get_file

--- src/app/services.py
+++ src/app/services.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile, Request, Response, Header
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from dotenv import dotenv_values
 from fastapi.responses import FileResponse
@@ -13,6 +14,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.post("/upload_file/")
 def upload_file(file: UploadFile = File(...)):
     pass
+
+@app.get("/get_file/{file_name}")
+def get_file(file_name: str):
+    file_path = "./static/image/" + file_name
+    return FileResponse(path=file_path)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8001)
