3c176daec7abb4a17dd6783a3314a6d53152a431
daominhkhanh20 <khanhc1k36@gmail.com>
2022-07-04 22:45:53 +0700

    update

--- src/utils/io.py
+++ src/utils/io.py
@@ -2,12 +2,14 @@ import json
 import logging
 import os
 import glob
+from matplotlib import pyplot as plt
+from collections import defaultdict
 
 logger = logging.getLogger(__name__)
 
 
 def save_config_architecture(data, path_file):
-    with open(path_file,'w') as file:
+    with open(path_file, 'w') as file:
         json.dump(data, file, indent=4)
     logger.info(f"Save architecture done in {path_file}")
 
@@ -23,3 +25,23 @@ def load_config_architecture(model_path):
         config = json.load(file)
 
     return config
+
+
+def plot_loss(history: defaultdict, path_save: str):
+    range_x = [i for i in range(len(history['train_loss']))]
+    plt.plot(range_x, history['train_loss'], label='train_loss')
+    plt.plot(range_x, history['val_loss'], label='val_loss')
+    plt.legend(loc="upper left")
+    plt.savefig(f'{path_save}/loss.png')
+    logger.info(f"Save loss.png at {path_save}")
+    plt.close()
+
+
+def plot_iou_score(history: defaultdict, path_save: str):
+    range_x = [i for i in range(len(history['train_iou']))]
+    plt.plot(range_x, history['train_iou'], label='train_iou')
+    plt.plot(range_x, history['val_iou'], label='val_iou')
+    plt.legend(loc="upper left")
+    plt.savefig(f'{path_save}/iou.png')
+    logger.info(f"Save iou.png at {path_save}")
+    plt.close()
